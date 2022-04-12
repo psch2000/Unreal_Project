bb38a756a0031d721e295192e24295caa45f7851
Dumling <ikk-noget@hotmail.com>
2022-04-12 15:06:23 +0200

    Loladins

---
+++ Source/Unreal_Project/Enemy.h
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "Enemy.generated.h"
+
+UCLASS()
+class UNREAL_PROJECT_API AEnemy : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Health")
+		int Health = 0;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Health")
+		int MaxHealth = 0;
+
+	//Functions
+	UFUNCTION(BlueprintCallable, Category = "Text")
+		void AIText();
+
+	// Sets default values for this character's properties
+	AEnemy();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
